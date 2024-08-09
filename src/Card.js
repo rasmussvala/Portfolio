fc8c99fbf4702e6109d087760760a8db11739673
rasmussvala <rasmus.svala@hotmail.com>
2024-08-09 17:26:41 +0200

    wip github link

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-function Card({ date, title, image: imagePath, description }) {
+function Card({ date, title, image: imagePath, description, github }) {
   const [image, setImage] = useState(null);
 
   useEffect(() => {
@@ -21,6 +23,16 @@ function Card({ date, title, image: imagePath, description }) {
       <div className="card-image-container">
         {image && <img src={image} alt={title} />}
       </div>
+      {github && (
+        <a
+          href={github}
+          className="github-project-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} style={{ color: "black" }} />
+        </a>
+      )}
       <header className="card-header">
         <p style={{ fontWeight: "bold" }}>{title}</p>
         <p>{date}</p>
